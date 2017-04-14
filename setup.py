908c730acd429facbbf745c9ebfe7e4d93cf55a5
Lilith Huang <lilithhuang17@gmail.com>
2017-04-14 10:40:50 -0700

    removed activation and inhibition labels from edges

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name = "jupyterPathway",
+    packages = ["jupyterPathway"],
+    version = "0.0.2",
+    description= "Canonical pathway visualization and analysis",
+    url = "https://github.com/ucsd-ccbb/jupyter-pathway",
+    author="Brin Rosenthal (sbrosenthal@ucsd.edu), Lilith Huang (lihuang@ucsd.edu)",
+    author_email="sbrosenthal@ucsd.edu",
+    keywords = ['Jupyter notebook', 'interactive', 'pathway', 'KEGG', 'metabolic'],
+    license = 'MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+    ]
+)
